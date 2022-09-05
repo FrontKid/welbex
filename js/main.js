0253c23bbc7f7b7650805ecc92130d108dbe5fdf
Denys Poltorak <itwfrontend@gmail.com>
2022-09-05 17:06:35 +0300

    second section of index was added + mobile

--- js/main.js
+++ js/main.js
@@ -1,24 +1,16 @@
 'use strict';
 function smooth() {
    let prevScrollpos = window.pageYOffset;
-
    const logoDesc = document.getElementById("logo__desc");
    const headerHeight = document.getElementById("header").offsetHeight
    const headerWidth = document.getElementById("header").offsetWidth
-
    const navbar = document.getElementById("navbar")
    const mobileNavInner = document.getElementById("mobile__nav-inner")
-
-
    if (prevScrollpos < 0) {
       mobileNavInner.style.display = 'none'
    }
-
-   window.onscroll = () => {
+   return window.onscroll = () => {
       const currentScrollPos = window.pageYOffset;
-
-      if (currentScrollPos === 0) {
-      }
       if (headerWidth > 800) {
          if (prevScrollpos > currentScrollPos) {
             navbar.style.top = "0";
@@ -36,13 +28,60 @@ function smooth() {
             mobileNavInner.style.display = 'flex'
          } else {
             mobileNavInner.style.display = 'none'
-
          }
          prevScrollpos = currentScrollPos;
       }
    }
-
 }
 
-
 smooth()
+
+
+function playVideo() {
+   const videoP = document.querySelector('#video__founder')
+   const btn = document.querySelector('#playBtnVideo')
+   const both = [videoP, btn]
+   return both.forEach(videoOrBtn => {
+      return videoOrBtn.addEventListener('click', () => {
+         const startVid = btn.getAttribute('data-video')
+
+         if (!(videoP.src).includes('founder')) {
+            videoP.src = startVid
+            videoP.play()
+            videoP.style.transform = 'scale(1.1)'
+            videoP.style.opacity = '1'
+            videoP.setAttribute('controls', "")
+         } else {
+            videoP.pause()
+            videoP.style.transform = 'scale(1)'
+            videoP.removeAttribute('controls')
+            videoP.src = ''
+         }
+      })
+   })
+
+}
+playVideo()
+function playMobVideo() {
+   const btn = document.getElementById('playBtnVideoMob')
+   const video = document.getElementById('video__mobile')
+
+   const playBoth = [btn, video]
+   return playBoth.forEach(elementToPlay => {
+      return elementToPlay.addEventListener("click", () => {
+         const attributeVideo = btn.getAttribute("data-video")
+
+         if (!video.src.includes("founder")) {
+            video.src = attributeVideo
+            video.play()
+            video.setAttribute("controls", "")
+            btn.style.opacity = "0"
+         } else {
+            video.src = ''
+            video.removeAttribute('controls')
+            btn.style.opacity = "1"
+         }
+      })
+   })
+}
+playMobVideo()
